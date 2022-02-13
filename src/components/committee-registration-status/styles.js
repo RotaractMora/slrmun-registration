3ac2754eb67fbf5c469ebc94f2d0b687f7bc11a9
Avishka-Perera <pereramat2000@gmail.com>
2022-02-13 07:32:50 +0530

    text to white

--- src/components/committee-registration-status/styles.js
+++ src/components/committee-registration-status/styles.js
@@ -1,16 +1,12 @@
 const styles = (theme, stat) => {
   // color selection
   let banner_color = theme.palette.error.main;
-  let text_color = theme.palette.error.contrastText;
   if (stat === 1 || stat === 2) {
     banner_color = theme.palette.orange.main;
-    text_color = theme.palette.orange.contrastText;
   } else if (stat === 3) {
     banner_color = theme.palette.yellow.main;
-    text_color = theme.palette.yellow.contrastText;
   } else if (stat === 4) {
     banner_color = theme.palette.success.main;
-    text_color = theme.palette.success.contrastText;
   }
   return {
     root: {
@@ -25,7 +21,7 @@ const styles = (theme, stat) => {
     },
     banner: {
       padding: "20px",
-      color: text_color,
+      color: "white",
       backgroundColor: banner_color,
     },
   };
