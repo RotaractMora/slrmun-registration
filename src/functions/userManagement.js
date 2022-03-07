e346ca50fdfe08456301b2f5f8d0bdb0332d9907
Avishka-Perera <pereramat2000@gmail.com>
2022-03-07 13:31:49 +0530

    updated the download table option

--- src/functions/userManagement.js
+++ src/functions/userManagement.js
@@ -29,6 +29,9 @@ export const usersObjectToCSV = (usersData, committeesData) => {
   const returnArr = [];
   for (let i = 0; i < usersData.length; i++) {
     const user = JSON.parse(JSON.stringify(usersData[i]));
+    if (user.name === "Avishka Perera") {
+      console.log(user);
+    }
 
     // change required data
     if (user.admin_approved) user.admin_approved = "Approved";
@@ -42,14 +45,37 @@ export const usersObjectToCSV = (usersData, committeesData) => {
     user.country = country;
     if (user.country_reserved) user.country_reserved = "Reserved";
     else user.country_reserved = "Not reserved";
-    if (!user.payment_slip) user.payment_slip = "";
-    if (!user.profile_picture) user.profile_picture = "";
+
+    if (user.payment_slip) {
+      user.payment_slip_time = timeStampToString(
+        user.payment_slip.timestamp,
+        1
+      );
+      user.payment_slip_url = user.payment_slip.public_url;
+    } else {
+      user.payment_slip_time = "";
+      user.payment_slip_url = "";
+    }
+
+    if (user.profile_picture) {
+      user.profile_picture_url = user.profile_picture.public_url;
+    } else {
+      user.profile_picture_url = "";
+    }
+
     user.current_status = getCurrentStatusFromNumber(user.current_status);
     if (user.interact_club === "") user.interact_club = "None";
     if (user.rotaract_club === "") user.rotaract_club = "None";
     user.user_level = userLevelToString(user.user_level);
-    user.payment_time = timeStampToString(user.payment_timestamp, 1);
     user.registered_time = timeStampToString(user.registered_timestamp, 1);
+    if (user.fps)
+      if (user.fps.file) user.fps = user.fps.file.public_url;
+      else user.fps = "";
+    else user.fps = "";
+
+    if (user.name === "Avishka Perera") {
+      console.log(user);
+    }
 
     // delete unwanted data
     delete user.payment_slip_storage_path;
@@ -60,13 +86,17 @@ export const usersObjectToCSV = (usersData, committeesData) => {
     delete user.rotaractor;
     delete user.payment_timestamp;
     delete user.registered_timestamp;
+    delete user.payment_slip;
+    delete user.profile_picture;
 
     //replace the commas with strings
     for (const key in user) {
       if (Object.hasOwnProperty.call(user, key)) {
         const value = user[key];
-        if (value.includes(",")) {
-          user[key] = value.replaceAll(",", "-");
+        if (typeof value === "string") {
+          if (value.includes(",")) {
+            user[key] = value.replaceAll(",", "-");
+          }
         }
       }
     }
@@ -81,5 +111,6 @@ export const usersObjectToCSV = (usersData, committeesData) => {
     const userRow = Object.values(orderedUser);
     returnArr.push(userRow);
   }
+  // console.log(returnArr);
   return returnArr;
 };
