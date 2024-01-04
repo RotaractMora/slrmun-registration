cf5def92f2514f0c6eee603eb56e1674f1050ad2
M.K. Akindu Kalhan <akinduk619@gmail.com>
2024-01-04 13:14:14 +0530

    Commented in Git Ignore

---
+++ src/constants/secrets.js
@@ -0,0 +1,7 @@
+export const FIREBASE_API_KEY =  "AIzaSyDEkgZGfXIWn2maHMjGDC3uM2K2TRCUDGw";
+export const FIREBASE_AUTH_DOMAIN =  "slrmun-24.firebaseapp.com";
+export const FIREBASE_DATABSE_URL =  "https://slrmun-24-default-rtdb.asia-southeast1.firebasedatabase.app";
+export const FIREBASE_PROJECT_ID =  "slrmun-24";
+export const FIREBASE_STORAGE_BUCKET =  "slrmun-24.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID =  "1091767194271";
+export const FIREBASE_APP_ID= "1:1091767194271:web:a0abfc27df30661a5cc8aa";
